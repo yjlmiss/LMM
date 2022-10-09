0447341b436d96771d494b34b497ae0a13003dbd
yjlmiss <yjl4022@gmail.com>
2022-10-09 08:04:16 +0800

    first commit

---
+++ vue-mm/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Header from "./components/Header.vue";
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <Header msg="You did it!" />
+
+     
+    </div>
+  </header>
+
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-mm/src/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  <div
+    style="
+      height: 50px;
+      line-height: 50px;
+      border-bottom: 1px solid #ccc;
+      display: flex;
+    "
+  >
+    <div
+      style="width: 200px; padding-left: 30px; font-weight: bold; color: peru"
+    >
+      后台管理
+    </div>
+    <div style="flex: 1"></div>
+    <div style="width: 100px">
+      <el-dropdown>
+        <span class="el-dropdown-link">
+          下拉菜单
+          <el-icon class="el-icon--right">
+            <arrow-down />
+          </el-icon>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>Action 1</el-dropdown-item>
+            <el-dropdown-item>Action 2</el-dropdown-item>
+            <el-dropdown-item>Action 3</el-dropdown-item>
+            <el-dropdown-item disabled>Action 4</el-dropdown-item>
+            <el-dropdown-item divided>Action 5</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" >
+
+export default {
+  name: "Header",
+};
+</script>
+<script  lang="ts" setup>
+
+import { ArrowDown } from '@element-plus/icons-vue';
+
+</script>
+
+<style scoped>
+.example-showcase .el-dropdown-link {
+  cursor: pointer;
+  color: var(--el-color-primary);
+  display: flex;
+  align-items: center;
+}
+</style>
